ff6e5d59c8a08a5ecb369e6dcc011122d3cd2232
YongKeeTan <yongkee.t@gmail.com>
2021-09-12 18:21:36 +0800

    adding untracked files

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,27 @@
+class BookmarksController < ApplicationController
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    # raise
+    # @list = List.find(params[:list_id])
+    # @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@bookmark.list)
+    else
+      render 'lists/index'
+    end
+  end
+
+  def destroy
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
