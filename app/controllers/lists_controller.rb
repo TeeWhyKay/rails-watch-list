0949f1fa2125364e8ce0872d16d82c1d68957487
YongKeeTan <yongkee.t@gmail.com>
2021-09-12 18:19:45 +0800

    implemented creation and deletion of bookmark

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,8 +4,28 @@ class ListsController < ApplicationController
   end
 
   def new
+    @list = List.new
   end
 
   def show
+    @list = List.find(params[:id])
+    @bookmarks = @list.bookmarks
+    @bookmark = Bookmark.new
+    @movies = @list.movies
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
